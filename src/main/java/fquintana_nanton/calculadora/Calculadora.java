c2c3b10a6021cab8eeee5c63416145d6decc57b9
nachoanton16 <nacho1a3@gmail.com>
2018-02-23 11:15:59 +0100

    Update data scanner

--- src/main/java/fquintana_nanton/calculadora/Calculadora.java
+++ src/main/java/fquintana_nanton/calculadora/Calculadora.java
@@ -1,5 +1,7 @@
 package fquintana_nanton.calculadora;
 
+import java.util.Scanner;
+
 /**
  * Write a description of class Calculadora here.
  * 
@@ -11,9 +13,9 @@ package fquintana_nanton.calculadora;
 
 public class Calculadora
 {
-    private int num1;
-    private int num2;
-    private int rdo;
+    private double num1;
+    private double num2;
+    private double rdo;
     private Operacion op;
 
     /**
@@ -31,17 +33,29 @@ public class Calculadora
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
         calc.opera();
-        int resultado = calc.dameResultado();
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introduce el primer número:");
+        
+        calc.ponNum1(sc.nextDouble());
+        
+        System.out.println("Introduce el segundo número:");
+        calc.ponNum2(sc.nextDouble());
+        
+
+        
+        double resultado = calc.dameResultado();
         System.out.println("Resultado: " + resultado);
     }
     
-    public void ponNum1(int n1)
+    public void ponNum1(double n1)
     {
         // put your code here
         this.num1=n1;
     }
     
-    public void ponNum2(int n2)
+    public void ponNum2(double n2)
     {
         // put your code here
         this.num2=n2;
@@ -81,17 +95,17 @@ public class Calculadora
             break;
         }
     }
-    public int dameResultado()
+    public double dameResultado()
     {
         // put your code here
         return rdo;
     }
     
     public void muestraTodosResultados(){
-        System.out.println("Num1="+Integer.toString(num1)+" Num2=" + Integer.toString(num2));
-        System.out.println("Suma :" + Integer.toString(num1+num2));
-        System.out.println("Resta :"+ Integer.toString(num1-num2));
-        System.out.println("Multiplica :" + Integer.toString(num1*num2));
-        System.out.println("Divide :"+ Integer.toString(num1/num2));
+        System.out.println("Num1="+Double.toString(num1)+" Num2=" + Double.toString(num2));
+        System.out.println("Suma :" + Double.toString(num1+num2));
+        System.out.println("Resta :"+ Double.toString(num1-num2));
+        System.out.println("Multiplica :" + Double.toString(num1*num2));
+        System.out.println("Divide :"+ Double.toString(num1/num2));
     }
 }
