e5e0e63a56b39ec08d3392b279263ae87626113d
nachoanton16 <nacho1a3@gmail.com>
2018-02-28 10:52:12 +0100

    Add pruebaResta

--- src/test/java/fquintana_nanton/calculadora/CalculadoraTest.java
+++ src/test/java/fquintana_nanton/calculadora/CalculadoraTest.java
@@ -26,4 +26,13 @@ public class CalculadoraTest {
         calculadora.opera();
         Assert.assertEquals(3, calculadora.dameResultado(), 0);
     }
+
+    @Test
+    public void pruebaResta() {
+        calculadora.ponNum1(1);
+        calculadora.ponNum2(2);
+        calculadora.ponOperacion("RESTA");
+        calculadora.opera();
+        Assert.assertEquals(-1, calculadora.dameResultado(), 0);
+    }
 }
