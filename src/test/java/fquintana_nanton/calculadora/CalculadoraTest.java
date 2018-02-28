ecbaca8a9a9ded20a5579df0889d7ff2b90ff590
franciscoquintana <francisco.quintana.dosa>
2018-02-28 11:01:20 +0100

    Añadidos Tests

--- src/test/java/fquintana_nanton/calculadora/CalculadoraTest.java
+++ src/test/java/fquintana_nanton/calculadora/CalculadoraTest.java
@@ -35,4 +35,22 @@ public class CalculadoraTest {
         calculadora.opera();
         Assert.assertEquals(-1, calculadora.dameResultado(), 0);
     }
+
+    @Test
+    public void pruebaMultiplicación() {
+        calculadora.ponNum1(1);
+        calculadora.ponNum2(2);
+        calculadora.ponOperacion("MULTIPLICA");
+        calculadora.opera();
+        Assert.assertEquals(2, calculadora.dameResultado(), 0);
+    }
+
+    @Test
+    public void pruebaDivision() {
+        calculadora.ponNum1(1);
+        calculadora.ponNum2(2);
+        calculadora.ponOperacion("DIVIDE");
+        calculadora.opera();
+        Assert.assertEquals(0.5, calculadora.dameResultado(), 0);
+    }
 }
